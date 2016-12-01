b682b6bbff184627e7e057d713c819f8ce8a15ec
Ching Yah Lan <EKMYYLChing@elken.com>
2016-12-01 17:21:09 +0800

    Finish

--- JC.php
+++ JC.php
@@ -11,6 +11,11 @@ foreach ($persons as $row)
    if(isset($row['contact_no'])){
        $row['Password']=md5($row['contact_no']);
    }
+   else
+   {
+
+       $row['Password']="";
+   }
 
     if($row['age']<18){
         unset($row['age']);
@@ -21,7 +26,6 @@ foreach ($persons as $row)
 
 
 if(isset($_GET['random']) && $_GET['random']==1){
-
     shuffle($newPerson);
 }
 
@@ -43,31 +47,7 @@ for ($row = 0; $row < 2; $row++) {
     echo "</ul>";
 }
 */
-?>
 
 
 
 
-//include the file "main.php" here
-
-//NOTE : we will have an array called $persons [refer to main], to print out the info and die, use dd($yourVar)
-// 1. Loop through all the array and add additional data to each person called password,
-// use md5 encryption as the password with the value from their mobile number.
-// 2. For those who doesn't have contact number, the password will be empty
-// 3. For those who are underage (18), remove the gender key from the array.
-// 4. Retrieve a GET parameter called random and limit
-//    If random is set to 1, randomize the array arrangement.
-//    If limit is set and more than 0, limit the length of the array to the limit set.
-//    Limit is only applicable if random is set to 1.
-
-/**
-* Useful readups :
-* http://php.net/manual/en/book.var.php
-* http://php.net/manual/en/language.references.php
-* http://php.net/manual/en/book.array.php
-*/
-
-
-//when its done, create [your name].html, include it here, and print out the data in a list or table. At the end of the table, add in the average age of this group.
-
-//last but not least
