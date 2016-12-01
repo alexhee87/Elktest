5e685da6881c38b806c3e70c7bc3d1bad93f6472
alex hee <alex.hee87@gmail.com>
2016-12-01 11:14:32 +0800

    adding git ignore file and index file

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by Alex Hee for Elken training
+ */
+
+?>
+
+<html>
+    <head>
+        <title>Welcome to GIT PHP Training</title>
+    </head>
+    <body>
+        <h1>Thank you for cloning me out. Welcome to the hands on PHP training.</h1>
+    </body>
+</html>
