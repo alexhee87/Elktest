40d10880345eeced8176631e68001719880b9761
alex hee <alex.hee87@gmail.com>
2016-12-01 14:21:54 +0800

    Add training and update index to load required files

--- index.php
+++ index.php
@@ -4,6 +4,19 @@
  * Created by Alex Hee for Elken training
  */
 
+//iterate through the directory from this path
+
+$dir = scandir('.');
+$arr = array();
+
+foreach($dir as $d){
+    $tmpName = explode('.', $d);
+    if(!isset($tmpName[1]) || $tmpName[1] != 'php' || in_array($tmpName[0], array('index','main','training'))){
+        continue;
+    }
+    $arr[] = $d;
+}
+
 ?>
 
 <html>
@@ -12,5 +25,12 @@
     </head>
     <body>
         <h1>Thank you for cloning me out. Welcome to the hands on PHP training.</h1>
+        <ul>
+        <?php foreach($arr as $dir){ ?>
+        <li>
+            <a href="<?php echo $dir; ?>"><?php echo $dir; ?></a>
+        </li>
+        <?php } ?>
+        </ul>
     </body>
 </html>
