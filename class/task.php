6287f5952620d5693538b6e680542b0b122a2167
alexhee <tshee@elken.com>
2016-12-22 11:03:59 +0800

    create task for week 5

---
+++ class/task.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Work in a pair. Create a system to read and write to database. 
+ * Create a system to crawl any site url. Get the html output and extract every single pictures 
+ * url in it. make sure the size is more than 150x150 pixel. Once you get it,
+ * Insert into database table of your choice. Create any number of classes to deal with the flow.
+ * Please work in pair and diversify the work. Create a folder of your team name and also a sql/txt
+ * file to import your db structure. Good luck!
+ * Extra : create a class/function to retrieve all the pictures from specific site with dimension
+ * parameter (minimum pixel), extension type, but  the additional filter is considered optional.
+ */
